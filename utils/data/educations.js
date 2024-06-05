95fd231d228e4be566584c5b0cb35f0aa4dc570f
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 11:32:58 +0000

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -14,7 +14,7 @@ export const educations = [
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2019",
+    duration: "208 - 2015",
     institution: "Debre Elias Secondary School",
   }
 ]
