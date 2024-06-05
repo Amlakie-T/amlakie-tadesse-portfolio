d1358de02a07e45e845976d467edf81c86e695a4
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 08:21:35 +0000

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,8 +12,25 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-          Copyright © 2024 All Rights Reserved to <a target="_blank" href="https://www.linkedin.com/in/amlakie-t/" class="text-[#16f2b3]">Amlakie Tadesse</a>.
+            Copyright © 2024 All Rights Reserved to <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
           </p>
+          <div className="flex items-center gap-5">
+            <Link
+              target="_blank"
+              href="https://github.com/said7388/developer-portfolio"
+              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
+            >
+              <IoStar />
+              <span>Star</span>
+            </Link>
+            <Link
+              target="_blank"
+              href="https://github.com/said7388/developer-portfolio/fork"
+              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
+            >
+              <CgGitFork />
+              <span>Fork</span>
+            </Link>
           </div>
         </div>
       </div>
