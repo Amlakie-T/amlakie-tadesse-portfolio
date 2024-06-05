234939e24004886d4b70df3f34c6cad9d396d31e
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 12:04:12 +0000

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Amlakie Tadesse",
   profile: '/amlakie_t.png',
   designation: "Software Developer",
-  description: "My name is Amlakie Tadesse. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  description: "I am Amlakie Tadesse, an experienced developer proficient in both mobile app and web development technologies. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. In mobile app development, I'm experienced with Java and Kotlin for native Android apps, as well as Flutter and React Native for cross-platform solutions. For web development, I specialize in JavaScript, utilizing powerful frameworks such as React.js, Angular, and Vue.js to create dynamic and responsive web applications. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'amlakietad@gmail.com',
   phone: '+251-912-416946',
   address: 'Addis Ababa, Ethiopia',
