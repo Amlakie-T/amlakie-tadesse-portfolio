07e37899755b3ccb0c382f4c8a9432d0c3e5e12a
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 07:13:24 +0000

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Software Developer',
+    company: "SS Communication PLC.",
+    duration: "(Jan 2024 - Present)"
   },
   {
     id: 2,
     title: "FullStack Developer",
     company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    duration: "(Jun 2021 - Present)"
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    duration: "(Jan 2020 - Present)"
   }
 ]
