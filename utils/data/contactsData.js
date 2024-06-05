312ceb2238fd83e9197f78574037514e02caf816
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 07:09:05 +0000

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
+    email: 'amlakietad@gmail.com',
+    phone: '+251-912-416946',
+    address: 'Addis Ababa, Ethiopia',
+    github: 'https://github.com/Amlakie-T',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
+    linkedIn: 'https://www.linkedin.com/in/amlakie-t/',
+    twitter: 'https://twitter.com/amlakie-t',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    devUsername: "Amlakie-T"
 }
