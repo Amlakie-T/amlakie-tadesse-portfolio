c8398c75e59fd8e8cd5aa148984fbc225e60007d
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 08:41:09 +0000

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+251-912-416946',
     address: 'Addis Ababa, Ethiopia',
     github: 'https://github.com/Amlakie-T',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    facebook: 'https://www.facebook.com/amlakie-t/',
     linkedIn: 'https://www.linkedin.com/in/amlakie-t/',
     twitter: 'https://twitter.com/amlakie-t',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/amlakie-t',
     devUsername: "Amlakie-T"
 }
