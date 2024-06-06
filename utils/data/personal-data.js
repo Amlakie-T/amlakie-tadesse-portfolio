f0acbdc228fd55083c1f47f8bc3318f7b6236f41
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 22:52:43 -1200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,7 +9,7 @@ export const personalData = {
   github: 'https://github.com/Amlakie-T',
   facebook: 'https://www.facebook.com/Amlakie-T/',
   linkedIn: 'https://www.linkedin.com/in/Amlakie-T/',
-  twitter: 'https://twitter.com/Amlakie-T',
+  twitter: 'https://twitter.com/Amlakie_T',
   stackOverflow: 'https://stackoverflow.com/users/25429126/amlakie-tadesse',
   leetcode: "https://leetcode.com/amlakie-t/",
   devUsername: "Amlakie-T",
