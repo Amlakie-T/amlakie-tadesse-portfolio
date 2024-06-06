a6ba09a9f7912461bab83e80724f7afbd53f123d
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 19:49:53 -1200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,10 +16,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description}/>
-      </Head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
