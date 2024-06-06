76f0bac6f0ebb5f4f306119bea1ee490117ad78c
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 21:14:34 -1200

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            Copyright © 2024 All Rights Reserved to <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Amlakie Tadesse</Link>
+            Copyright © 2024 All Rights Reserved to <Link target="_blank" href="https://www.linkedin.com/in/amlakie/" className="text-[#16f2b3]">Amlakie Tadesse</Link>
           </p>
     
         </div>
