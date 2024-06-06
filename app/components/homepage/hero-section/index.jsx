a42cfbd3bc298fa7bb08f24e4a452d12344dc885
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-05 22:07:25 -1200

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -176,8 +176,8 @@ function HeroSection() {
               <div>
                 <span className="ml-12 lg:ml-24 text-cyan-400">this.</span>
                 <span className="mr-2 text-white">skills.length</span>
-                <span className="mr-2 text-amber-300">&gt;</span>
-                <span className="text-orange-400">=7</span>
+                <span className="mr-2 text-amber-300">&gt;=</span>
+                <span className="text-orange-400">7</span>
               </div>
               <div><span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span></div>
               <div><span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span></div>
