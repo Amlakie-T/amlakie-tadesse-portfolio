8bb0c12b04d98455b5f6fe3b1775461fdd3dd572
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-06 16:33:45 +0300

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -16,26 +16,6 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
-    },
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
         name: 'Newsroom Management',
         description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
         tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
@@ -44,18 +24,8 @@ export const projectsData = [
         image: ayla,
         role: 'Full Stack Developer',
     },
-        {
-        id: 5,
-        name: 'Android POS Application',
-        description: "My team and I developed an Android POS (Point of Sale) application tailored for various banks in Ethiopia. As a full-stack developer, I contributed to the development of the application's frontend and backend functionalities. On the frontend, I focused on crafting a user-friendly interface using Java and XML. For backend operations, I utilized technologies such as SQLite for local data storage and integration with bank APIs for transaction processing and authorization.",
-        tools: ['Java', 'Android SDK', 'XML', 'SQLite', 'OkHttp', 'Payment Gateway Integration'],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    },
     {
-        id: 6,
+        id: 3,
         name: 'IOT Based Android Application',
         description: "My team and I developed an IoT-based Android application focused on smart home management. As a full-stack developer, I contributed to building the app's frontend and backend functionalities. Using Java for Android development, I integrated IoT sensors and devices, enabling users to monitor and control their home environment remotely. We utilized Firebase for backend services, integrating real-time data syncing and authentication. Additionally, I implemented UI/UX design principles to ensure a seamless user experience.",
         tools: ['Java', 'C', 'XML', 'SQLite', 'Arduino', 'Android Studio', 'Firebase', 'IoT Sensors'],
@@ -63,8 +33,17 @@ export const projectsData = [
         demo: '',
         image: ayla,
         role: 'Full Stack Developer',
-   }
-    
+   },
+    {
+        id: 4,
+        name: 'Android POS Application',
+        description: "My team and I developed an Android POS (Point of Sale) application tailored for various banks in Ethiopia. As a full-stack developer, I contributed to the development of the application's frontend and backend functionalities. On the frontend, I focused on crafting a user-friendly interface using Java and XML. For backend operations, I utilized technologies such as SQLite for local data storage and integration with bank APIs for transaction processing and authorization.",
+        tools: ['Java', 'Android SDK', 'XML', 'SQLite', 'OkHttp', 'Payment Gateway Integration'],
+        code: '',
+        demo: '',
+        image: ayla,
+        role: 'Full Stack Developer',
+    }
 ];
 
 
