3dfcbd9bbee3a6c5b198eb1fc5e58f7245a07a15
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-07 16:46:31 +0300

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -10,48 +10,6 @@ import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 
 function HeroSection() {
-  const textArray = [
-    "Developer",
-    "Software Engineer",
-    "Frontend Developer",
-    "Backend Developer"
-  ];
-
-  let index = 0;
-  let text = "";
-  let isDeleting = false;
-
-  function type() {
-    const currentText = textArray[index];
-    if (isDeleting) {
-      text = currentText.substring(0, text.length - 1);
-    } else {
-      text = currentText.substring(0, text.length + 1);
-    }
-
-    document.getElementById('typing-text').innerHTML = text;
-
-    let typeSpeed = 100; // Adjust typing speed here
-
-    if (isDeleting) {
-      typeSpeed /= 2; // Adjust deleting speed here
-    }
-
-    if (!isDeleting && text === currentText) {
-      typeSpeed = 1000; // Pause at end, adjust as needed
-      isDeleting = true;
-    } else if (isDeleting && text === '') {
-      isDeleting = false;
-      index++;
-      if (index === textArray.length) {
-        index = 0;
-      }
-    }
-
-    setTimeout(type, typeSpeed);
-  }
-
-  window.onload = type;
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
       <Image
@@ -72,9 +30,7 @@ function HeroSection() {
             <span className=" text-[#16f2b3]">{personalData.designation}</span>
             .
           </h1>
-          <div>
-       <h1 id="typing-text" className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem] text-[#16f2b3]"></h1>
-            </div>
+
           <div className="my-12 flex items-center gap-5">
             <Link
               href={personalData.github}
