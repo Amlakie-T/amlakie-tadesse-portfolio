2c12a117a9272ac2d8e5afe0732ff209ebcd7f88
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-07 13:19:24 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-
+import React, { useState, useEffect, useRef } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
