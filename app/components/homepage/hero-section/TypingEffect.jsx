3023613c5556371260d339f17d8ffc81443a4fba
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-08 10:04:44 +0300

    Update TypingEffect.jsx

--- app/components/homepage/hero-section/TypingEffect.jsx
+++ app/components/homepage/hero-section/TypingEffect.jsx
@@ -1,79 +1,48 @@
-"use client";
+import React from "react";
+import Typewriter from "typewriter-effect";
 
-import React, { useEffect, useState, useCallback } from 'react';
+function TypingEffect() {
+    return (
+        <div>
+            <h1 id="typing-text">
+                <Typewriter
+                    options={{
+                        strings: [
+                            "Developer",
+                            "Software Engineer",
+                            "Frontend Developer",
+                            "Backend Developer",
+                        ],
+                        autoStart: true,
+                        loop: true,
+                        delay: 150, // Slower typing speed
+                        deleteSpeed: 100, // Slower deleting speed
+                        pauseFor: 2000, // Pause before deleting
+                    }}
+                />
+                <span className="typing-cursor" style={{ width: '5px' }}></span>
+            </h1>
+            <style>{`
+                .typing-cursor {
+                    display: inline-block;
+                    width: 4px; /* Adjust cursor width */
+                    height: 25px; /* Adjust cursor height */
+                    background-color: #b30059; /* Adjust cursor color */
+                    animation: blink-animation 1.2s infinite;
+                    font-weight: 600; /* Adjust cursor weight */
+                }
 
-const TypingEffect = () => {
-  const textArray = [
-    'Developer',
-    'Software Engineer',
-    'Frontend Developer',
-    'Backend Developer',
-  ];
-
-  const [index, setIndex] = useState(0);
-  const [text, setText] = useState('');
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [waitingTime, setWaitingTime] = useState(1000); // Customizable waiting time
-
-  const type = useCallback(() => {
-    const currentText = textArray[index];
-    setText((prevText) =>
-      isDeleting
-        ? currentText.substring(0, prevText.length - 1)
-        : currentText.substring(0, prevText.length + 1)
+                @keyframes blink-animation {
+                    0%, 100% {
+                        opacity: 1;
+                    }
+                    50% {
+                        opacity: 0;
+                    }
+                }
+            `}</style>
+        </div>
     );
-
-    const typeSpeed = 50; // Adjust typing speed
-
-    if (!isDeleting && text === currentText) {
-      setTimeout(() => {
-        setIsDeleting(true);
-      }, typeSpeed); // Wait before deleting
-    } else if (isDeleting && text === '') {
-      setIsDeleting(false);
-      const nextIndex = (index + 1) % textArray.length;
-
-      setTimeout(() => {
-        setIndex(nextIndex);
-        type();
-      }, waitingTime); // Consistent waiting time after deletion
-    } else {
-      setTimeout(type, typeSpeed);
-    }
-  }, [text, isDeleting, index, textArray, waitingTime]);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(type, 500);
-    return () => clearTimeout(timeoutId);
-  }, [type]);
-
-  return (
-    <div>
-      <h1 id="typing-text">
-        <span style={{ color: '#16f2b3', fontWeight: 500 }}>{text}</span>
-        <span className="typing-cursor" style={{ width: '5px' }}></span>
-      </h1>
-      <style>{`
-        .typing-cursor {
-          display: inline-block;
-          width: 5px; /* Adjust cursor width */
-          height: 25px; /* Adjust cursor height */
-          background-color: #b30059; /* Adjust cursor color */
-          animation: blink-animation 1.2s infinite;
-          font-weight: 600; /* Adjust cursor weight */
-        }
-
-        @keyframes blink-animation {
-          0%, 100% {
-            opacity: 1;
-          }
-          50% {
-            opacity: 0;
-          }
-        }
-      `}</style>
-    </div>
-  );
-};
+}
 
 export default TypingEffect;
