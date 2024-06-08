eaff15f9046a6d381c867bc4a3b0dc5dd4355046
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-08 10:15:52 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Amlakie Tadesse",
   profile: '/amlakie_t.png',
-  designation: "Software Developer",
+  designation: "Full Stack Developer",
   description: "I'm Amlakie Tadesse, an experienced developer. I work with mobile apps using Java, Kotlin, Flutter, and React Native, and for websites, I use JavaScript with frameworks like React.js, Angular, and Vue.js to create dynamic and responsive web applications. I love solving problems and quickly learning new tech. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'amlakietad@gmail.com',
   phone: '+251-912-416946',
