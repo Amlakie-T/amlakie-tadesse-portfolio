58abee09169c9fa42f779e174b1cc6b50d4c3803
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-08 10:12:24 +0300

    Update TypingEffect.jsx

--- app/components/homepage/hero-section/TypingEffect.jsx
+++ app/components/homepage/hero-section/TypingEffect.jsx
@@ -22,27 +22,7 @@ function TypingEffect() {
                         pauseFor: 2000, // Pause before deleting
                     }}
                 />
-                <span className="typing-cursor" style={{ width: '5px' }}></span>
             </h1>
-            <style>{`
-                .typing-cursor {
-                    display: inline-block;
-                    width: 4px; /* Adjust cursor width */
-                    height: 25px; /* Adjust cursor height */
-                    background-color: #b30059; /* Adjust cursor color */
-                    animation: blink-animation 1.2s infinite;
-                    font-weight: 600; /* Adjust cursor weight */
-                }
-
-                @keyframes blink-animation {
-                    0%, 100% {
-                        opacity: 1;
-                    }
-                    50% {
-                        opacity: 0;
-                    }
-                }
-            `}</style>
         </div>
     );
 }
