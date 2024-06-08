00e3f04da8ab2cb738646bf8fae92addee96c28b
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-08 11:02:57 +0300

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -13,6 +13,44 @@ function Footer() {
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
             Copyright © 2024 All Rights Reserved to <Link target="_blank" href="https://www.linkedin.com/in/amlakie/" className="text-[#16f2b3]">Amlakie Tadesse</Link>
+          
+          <div className="my-12 flex items-center gap-5">
+            <Link
+              href={personalData.github}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <BsGithub size={30} />
+            </Link>
+            <Link
+              href={personalData.linkedIn}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <BsLinkedin size={30} />
+            </Link>
+            <Link
+              href={personalData.facebook}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <FaFacebook size={30} />
+            </Link>
+            <Link
+              href={personalData.leetcode}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <SiLeetcode size={30} />
+            </Link>
+            <Link
+              href={personalData.twitter}
+              target='_blank'
+              className="transition-all text-pink-500 hover:scale-125 duration-300"
+            >
+              <FaTwitterSquare size={30} />
+            </Link>
+          </div>
           </p>
     
         </div>
