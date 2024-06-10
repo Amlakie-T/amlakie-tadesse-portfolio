c518f729fc592a5d32a6c1f81526b4bcd67b944d
Amlakie Tadesse <biniamtaddesse@gmail.com>
2024-06-10 13:48:29 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,7 +8,7 @@ export const personalData = {
   address: 'Addis Ababa, Ethiopia',
   github: 'https://github.com/Amlakie-T',
   facebook: 'https://www.facebook.com/Amlakie-T/',
-  linkedIn: 'https://www.linkedin.com/in/Amlakie-T/',
+  linkedIn: 'https://www.linkedin.com/in/amlakie/',
   twitter: 'https://twitter.com/Amlakie_T',
   stackOverflow: 'https://stackoverflow.com/users/25429126/amlakie-tadesse',
   leetcode: "https://leetcode.com/amlakie-t/",
