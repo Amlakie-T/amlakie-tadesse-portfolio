0acaaa8f0378e617c58a72204194c8f01f706eaf
Amlakie Tadesse <biniamtaddesse@gmail.com>
2025-05-03 12:56:21 +0300

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,5 @@
-import blogList from '../../components/homepage/blog/blogList'
-import { getPosts } from '../../utils/data/blogData'
-
+import blogList from '@/components/homepage/blog/blogList'
+import { getPosts } from '@/utils/data/blogData'
 export default function blogPage() {
   const posts = getPosts()
   return <blogList posts={posts} />
