9f4f6e31dafa65f02e0fe21fca0dfd99ab3d0643
Amlakie Tadesse <biniamtaddesse@gmail.com>
2025-05-03 12:52:36 +0300

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,18 +1,7 @@
-// app/blog/page.js
-import BlogIndex from '@/components/blog';
-import { getBlogPosts } from '@/utils/data/blogPost';
+import blogList from '../../components/homepage/blog/blogList'
+import { getPosts } from '../../utils/data/blogData'
 
-export async function generateMetadata() {
-  return {
-    title: 'Tech Blog | Android, Web & IT Insights',
-    description: 'Expert articles on Android development, Web technologies, and IT support solutions',
-    openGraph: {
-      images: ['/og-blog.jpg'],
-    },
-  };
-}
-
-export default async function BlogPage() {
-  const posts = await getBlogPosts();
-  return <BlogIndex posts={posts} />;
+export default function blogPage() {
+  const posts = getPosts()
+  return <blogList posts={posts} />
 }
