82ae35bf159ae7a856ae7f7cd1e33d1b76548b93
Amlakie Tadesse <biniamtaddesse@gmail.com>
2025-05-03 12:47:04 +0300

    Update blogData.js

--- utils/data/blogData.js
+++ utils/data/blogData.js
@@ -1,1 +1,15 @@
+const posts = [
+  {
+    title: "Kotlin Coroutines Guide",
+    excerpt: "Learn async programming in Android",
+    image: "/public/image/android-coroutines.jpg",
+    tags: ["android"],
+    slug: "kotlin-guide",
+    date: "2023-11-15"
+  }
+  // Add more posts...
+]
 
+export function getPosts() {
+  return posts.sort((a, b) => new Date(b.date) - new Date(a.date))
+}
