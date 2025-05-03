3877b106a13fa768e8b3939e46130057ab1e948c
Amlakie Tadesse <biniamtaddesse@gmail.com>
2025-05-03 10:58:32 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,9 +1,9 @@
 export const personalData = {
   name: "Amlakie Tadesse",
   profile: '/amlakie_t.png',
-  designation: "Full Stack Developer and Mobile App Specialist",
-  description: "I'm Amlakie, an experienced Full Stack Developer and Mobile App Specialist adept at creating robust solutions for both mobile apps and websites. Skilled in Android development using Java, Kotlin, and Flutter. Proficient in front-end technologies (HTML, CSS, JavaScript with React.js) and back-end development with Node.js and PHP. Known for problem-solving, mastering new technologies quickly, and collaborating effectively to deliver innovative projects. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'amlakietad@gmail.com',
+  designation: "Android Developer | Web Developer | IT Support Engineer",
+  description: "I'm Amlakie, an experienced Android Developer and Web Developer adept at creating robust solutions for both mobile apps and websites. Skilled in Android development using Java, Kotlin, and Flutter. Proficient in front-end technologies (HTML, CSS, JavaScript with React.js) and back-end development with Node.js and PHP. Known for problem-solving, mastering new technologies quickly, and collaborating effectively to deliver innovative projects. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'amlakie.tadese@gmail.com',
   phone: '+251-912-416946',
   address: 'Addis Ababa, Ethiopia',
   github: 'https://github.com/Amlakie-T',
