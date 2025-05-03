e4df8f520d2017efaa6f24acebad9d973289738b
Amlakie Tadesse <biniamtaddesse@gmail.com>
2025-05-03 12:33:11 +0300

    Update page.js

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,44 +1,18 @@
-// @flow strict
+// app/blog/page.js
+import BlogIndex from '@/components/blog';
+import { getBlogPosts } from '@/utils/data/blogPost';
 
-import { personalData } from "@/utils/data/personal-data";
-import BlogCard from "../components/homepage/blog/blog-card";
+export async function generateMetadata() {
+  return {
+    title: 'Tech Blog | Android, Web & IT Insights',
+    description: 'Expert articles on Android development, Web technologies, and IT support solutions',
+    openGraph: {
+      images: ['/og-blog.jpg'],
+    },
+  };
+}
 
-async function getBlogs() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-  return data;
-};
-
-async function page() {
-  const blogs = await getBlogs();
-
-  return (
-    <div className="py-8">
-      <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            All Blog
-          </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
-        {
-          blogs.map((blog, i) => (
-            blog?.cover_image &&
-            <BlogCard blog={blog} key={i} />
-          ))
-        }
-      </div>
-    </div>
-  );
-};
-
-export default page;
+export default async function BlogPage() {
+  const posts = await getBlogPosts();
+  return <BlogIndex posts={posts} />;
+}
